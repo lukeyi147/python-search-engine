96e8a25a0c6dee2ed9910efd26ec7f718cac6069
Hossein Haider <hosseinhaider@gmail.com>
2014-10-30 16:22:39 -0400

    added unit tests in crawler_test.py

--- lab1/crawler_test.py
+++ lab1/crawler_test.py
@@ -11,8 +11,8 @@ class IsOk(unittest.TestCase):
 		inverted_index = bot.inverted_index()
 		resolved_inverted_index = bot.resolved_inverted_index()
 
-		expected_inverted_index = bot.inverted_index()
-		expected_resolved_inverted_index = bot.resolved_inverted_index()
+		expected_inverted_index = {1: set([1, 2]), 2: set([1, 2])}
+		expected_resolved_inverted_index = {u'index2': set(['http://hhaider.github.io/mygithubpage/index.html', u'http://hhaider.github.io/mygithubpage/index2.html']), u'index': set(['http://hhaider.github.io/mygithubpage/index.html', u'http://hhaider.github.io/mygithubpage/index2.html'])}
 
 		self.failUnless(inverted_index == expected_inverted_index)
 		self.failUnless(resolved_inverted_index == expected_resolved_inverted_index)
