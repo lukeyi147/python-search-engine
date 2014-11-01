28071f1b860effe6ce89febaafd0c3e326aa340e
Hossein Haider <hosseinhaider@gmail.com>
2014-10-31 20:02:33 -0400

    added session managment with beaker

--- lab2/server.py
+++ lab2/server.py
@@ -4,60 +4,68 @@ from oauth2client.client import OAuth2WebServerFlow
 from oauth2client.client import flow_from_clientsecrets
 from googleapiclient.errors import HttpError
 from googleapiclient.discovery import build
+from beaker.middleware import SessionMiddleware
 import bottle
 import httplib2
 
-#History dictionary contains (word, count) pairs for all words entered in field
+# contains {user_email:[history]} dict
 hist = {}
 
-#Flag for indicating signed in status
-signed = 0
+# init beaker file based session
+session_opts = {
+    'session.type': 'file',
+    'session.data_dir': './session/',
+    'session.auto': True,
+}
 
-#The email of the signed in user
-user_email = None
-                                
+app = SessionMiddleware(bottle.app(), session_opts)
+     
 @route('/', method='GET')
-
 def engine():
-    global signed
-    global user_email
+    # get session
+    session = bottle.request.environ['beaker.session']
+    signed = 'user_email' in session
+    if signed:
+        user_email = session['user_email']
+
+    # handle sign_in
     if request.GET.get('sign_in',''):
-        signed = 1
+        # flow step1
         flow = flow_from_clientsecrets("client_secret_864736965004-d6snoqp3e8d97nh17c9b7pgisaqhjff2.apps.googleusercontent.com.json",
             scope='https://www.googleapis.com/auth/plus.me https://www.googleapis.com/auth/userinfo.email',
             redirect_uri="http://localhost:8080/redirect")
         uri = flow.step1_get_authorize_url()
         bottle.redirect(str(uri))
         
-        
+    # handle sign_out
     if request.GET.get('sign_out',''):
-        signed = 0
+        session.delete()
         bottle.redirect("http://localhost:8080")
         
+    # handle submit
     if request.GET.get('submit',''):
-    #User has submitted keywords
-
-        global hist
-
-        #Result dictionary contains (word, count) info of current keywords entered by user
+ 
+        # result dictionary contains (word, count) info of current keywords entered by user
         results = {}
 
-        #Get keywords from user and split by whitespace into keywords list
+        # get keywords from user and split by whitespace into keywords list
         keywords = request.query['keywords'].split(" ")
+
         #############################
-        #Iterate over keywords entered by user
+        # iterate over keywords entered by user
         for keyword in keywords:
 
-            #Ignore whitespace keywords
+            # ignore whitespace keywords
             if keyword != "":
 
-                #Insert keyword into current results, else increment value
+                # insert keyword into current results, else increment value
                 if not keyword in results:
                     results[keyword] = 1
                 else:
                     results[keyword] += 1
 
         ##################
+        global hist
         if signed == 1:
             if user_email not in hist:    #if user not in dictionary
                 hist.update({user_email:[]})
@@ -77,13 +85,12 @@ def engine():
             print history
                 
         if signed == 1 :return template('index', results = results, history = history, sgn = signed, u_email = user_email)
-        if signed == 0 :return template('index', results = results, history = 0, sgn = signed, u_email = user_email)
+        if signed == 0 :return template('index', results = results, history = 0, sgn = signed, u_email = 0)
 
     else:
-    #User didn't submit keywords, display empty results, and display top 20 history
-
+    # user didn't submit keywords, display empty results
         results = {}        
-        if signed == 1 :
+        if signed == 1:
             if user_email not in hist:    #if user not in dictionary
                 hist.update({user_email:[]})
 
@@ -92,11 +99,15 @@ def engine():
             history.reverse()
 
             return template('index', results = results, history = history, sgn = signed, u_email = user_email)
-        if signed == 0 :return template('index', results = results, history = 0, sgn = signed, u_email = user_email)
+        if signed == 0: 
+            return template('index', results = results, history = 0, sgn = signed, u_email = 0)
         
 @route('/redirect', method='GET')
 def redirect_page():
-    global user_email    
+    # get session
+    session = bottle.request.environ['beaker.session']
+
+    # flow step2
     code = request.query.get('code', '')
     flow = flow_from_clientsecrets("client_secret_864736965004-d6snoqp3e8d97nh17c9b7pgisaqhjff2.apps.googleusercontent.com.json",
         scope='https://www.googleapis.com/auth/plus.me https://www.googleapis.com/auth/userinfo.email',
@@ -107,13 +118,15 @@ def redirect_page():
     http = httplib2.Http()
     http = credentials.authorize(http)
     
-    #Get user email
+    # get user email and store in session
     users_service = build('oauth2', 'v2', http=http)
     user_document = users_service.userinfo().get().execute()
-    user_email = user_document['email']    
+    session['user_email'] = user_document['email']
+    session.save()
+
     bottle.redirect("http://localhost:8080")
     
     
-run(host='localhost', port=8080, debug=True)
+run(app = app, host='localhost', port=8080, debug=True)
 
 
